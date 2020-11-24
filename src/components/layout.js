48386ef79876ab9da7dc4867b43949501a0e45a2
Giuseppe <giuseppemiro@gmail.com>
2020-11-24 06:55:39 +0100

    add social buttons to the footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,17 +2,19 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import Header from "./header";
+import SocialButtons from "./socialButtons";
 
 function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen font-sans text-gray-900 bg-gray-800">
       <Header />
 
-      <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16 bg-white rounded">
+      <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-8 bg-white rounded">
         {children}
       </main>
 
       <footer className="bg-gray-800">
+      <SocialButtons></SocialButtons>
       <div className="text-white mt-5 text-center ">  This blog is part of my{" "}
             <a
               href="https://twitter.com/ggangix/status/1328031248808349696?s=20"
@@ -22,8 +24,8 @@ function Layout({ children }) {
               #100DaysOfCode
             </a>{" "}
             challenge.</div>
-        <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
           
+        <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
         </nav>
       </footer>
     </div>
