6be78dd75dcd99dbb85f32748cf794bd8061bce4
Giuseppe <giuseppemiro@gmail.com>
2020-11-24 06:55:09 +0100

    Add social buttons component

---
+++ src/components/socialButtons.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SocialButtons() {
+  return (
+    <div className="space-x-4 mt-5 text-xl min-w-full text-center">
+      <a href="https://github.com/ggangix" target="blank" className="fa fa-github inline-block text-white  hover:text-gray-600"></a>
+      <a href="https://www.facebook.com/ggangix" target="blank" className="fa fa-facebook inline-block text-white hover:text-gray-600"></a>
+      <a href="https://twitter.com/ggangix" target="blank" className="fa fa-twitter inline-block  text-white  hover:text-gray-600"></a>
+      <a href="https://www.instagram.com/giuseppe.gangi/" target="blank" className="fa fa-instagram inline-block  text-white  hover:text-gray-600"></a>
+    </div>
+  );
+}
+
+export default SocialButtons;
