fbfbf372ae8a5ca94200fbf2635b29244350ff19
Giuseppe <giuseppemiro@gmail.com>
2020-11-25 17:53:42 +0100

    Add component to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import VerticalTimeline from "../components/verticalTimeline";
 
 function AboutPage() {
   return (
@@ -11,10 +12,12 @@ function AboutPage() {
         title="About"
       />
 
-      <section className="flex flex-col items-center md:flex-row">
-         Giuseppe interesting life...
+      <section className="flex flex-col items-center md:flex-row m-8">
+      <p>My name is Giuseppe and I am IT Enginner, currently working in Germany as a Software Developer at Foxxum GmbH. I was born in Venezuela in an immigrant family from Italy and Colombia.</p>  
 
       </section>
+  
+      <VerticalTimeline></VerticalTimeline>
     </Layout>
   );
 }
