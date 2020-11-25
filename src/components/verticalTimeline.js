55b9ad3949ff123522e9a17bdea435728083430d
Giuseppe <giuseppemiro@gmail.com>
2020-11-25 17:52:31 +0100

    Add vertical timeline component

---
+++ src/components/verticalTimeline.js
@@ -0,0 +1,29 @@
+import React from "react";
+import timeline from '../data/timeline.json'
+
+function VerticalTimeline() {
+  return (
+    <div className="relative m-8">
+      <div className="border-r-2 border-gray-500 absolute h-full top-0"></div>
+      <ul className="list-none m-0 p-0">
+        {timeline.map((current, index) => (
+        <li className="mb-10" key={index}>
+          <div className="flex items-center mb-1">
+            <div className="bg-gray-500 rounded-full h-8 w-8 text-xl text-center">
+              <p className={`fa ${current.icon} text-white relative`}></p>
+            </div>
+            <div className="ml-4 font-medium">{`${current.title}`} </div>
+            <div className="ml-4 mt-1 text-xs font-mono text-gray-600">{`${current.date}`} </div>
+          </div>
+          <div className="ml-12">
+            {current.description}
+         </div>
+        </li>  
+        ))}
+
+      </ul>
+    </div>
+  );
+}
+
+export default VerticalTimeline;
