48e7c9f951847341b95db9d094066578ebf11ea4
Giuseppe <giuseppemiro@gmail.com>
2020-11-26 19:23:23 +0100

    Complete about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,11 +12,33 @@ function AboutPage() {
         title="About"
       />
 
-      <section className="flex flex-col items-center md:flex-row m-8">
-      <p>My name is Giuseppe and I am IT Enginner, currently working in Germany as a Software Developer at Foxxum GmbH. I was born in Venezuela in an immigrant family from Italy and Colombia.</p>  
-
+      <section className="items-center m-8">
+        <p className="mb-4">
+          My name is Giuseppe and I am IT Enginner, currently working in Germany
+          as a Software Developer at Foxxum GmbH. I was born in Venezuela in an
+          immigrant family from Italy and Colombia.
+        </p>
+        <p className="mb-4">
+          I have been working in jobs related with computers and code since I
+          was a teenager, thanks to that I was able to take care of me and my
+          family since early ages in a really unstable country, and for this
+          reason my vision about the life and work is always taking
+          responsabilities and working step by step but with the future goal in
+          mind.
+        </p>
+        <p className="mb-4">
+          This is a brief summary about my professional life in the last years.
+          If you have any question, please feel free to{" "}
+          <a
+            className="font-bold underline hover:no-underline"
+            href="mailto:me@giuseppe.xyz"
+          >
+            Contact me
+          </a>
+          .
+        </p>
       </section>
-  
+
       <VerticalTimeline></VerticalTimeline>
     </Layout>
   );
